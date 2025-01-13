9bd5fda94e3b303add1860afe00e8574e254b08d
behruz20120106 <behruzbakhodirhodjaev12@gmail.com>
2025-01-13 21:42:46 +0500

    script.js

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ let age;
 document.getElementById(`submitBTN`).onclick = function(){
     username = document.getElementById(`username`).value;
     age = document.getElementById(`age`).value;
-    document.getElementById(`myH1`).textContent = `Happy birthday ${username}!`
-    document.getElementById(`myH2`).textContent = `You are ${age} years old!`
+    document.getElementById(`myH1`).textContent = `Happy birthday ${username}!`;
+    document.getElementById(`myH2`).textContent = `You are ${age} years old!`;
 }
 
 function happyBirthday(){
-    console.log("Happy birthday to you!")
-    console.log("Happy birthday to you!")
-    console.log(`Happy birthday dear ${`username`}`)
-    console.log("Happy birthday to you!")
-    console.log(`You are ${`age`} !`)
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday to you!");
+    console.log(`Happy birthday dear ${`username`}`);
+    console.log("Happy birthday to you!");
+    console.log(`You are ${`age`} !`);
+}
+happyBirthday()
+
+document.getElementById(`reset`).onclick = function(){ 
+    document.getElementById(`myH1`).textContent = `Welcome`;
+    document.getElementById(`myH2`).textContent = `Please fill the blanks`;
 }
